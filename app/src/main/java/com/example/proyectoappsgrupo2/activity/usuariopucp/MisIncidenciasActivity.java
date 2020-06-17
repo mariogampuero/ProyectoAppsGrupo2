fb16b449a81a2837be4e8a5468b4f3230b9152b8
ruthnadiezhda <a20150875@pucp.edu.pe>
2020-06-17 16:28:46 -0500

    vistas ahora si del usuario puke

---
+++ app/src/main/java/com/example/proyectoappsgrupo2/activity/usuariopucp/MisIncidenciasActivity.java
@@ -0,0 +1,16 @@
+package com.example.proyectoappsgrupo2.activity.usuariopucp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyectoappsgrupo2.R;
+
+public class MisIncidenciasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mis_incidencias);
+    }
+}
