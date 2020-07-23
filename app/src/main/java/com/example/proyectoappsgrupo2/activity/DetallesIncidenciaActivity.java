10a859fcd65b66ec667d8d544bc102519add5e74
Mario Ampuero <m.ampuero@pucp.pe>
2020-07-22 22:01:07 -0500

    Se añadió el mapa a la vista de detalles

--- app/src/main/java/com/example/proyectoappsgrupo2/activity/DetallesIncidenciaActivity.java
+++ app/src/main/java/com/example/proyectoappsgrupo2/activity/DetallesIncidenciaActivity.java
@@ -1,16 +1,36 @@
 package com.example.proyectoappsgrupo2.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
 import com.example.proyectoappsgrupo2.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class DetallesIncidenciaActivity extends AppCompatActivity {
+public class DetallesIncidenciaActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles_incidencia);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng latLng = new LatLng(-12.060896, -77.041357);
+        map.addMarker(new MarkerOptions().position(latLng).title("Incidencia 1"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
     }
 }
