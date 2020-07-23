2e62219b18a71a90c7eb060ee8c861c39bb9ec7e
Mario Ampuero <m.ampuero@pucp.pe>
2020-07-22 19:53:43 -0500

    Se añadió un paquete faltante a una clase

--- app/src/main/java/com/example/proyectoappsgrupo2/activity/DetallesIncidenciaActivity.java
+++ app/src/main/java/com/example/proyectoappsgrupo2/activity/DetallesIncidenciaActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.proyectoappsgrupo2.R;
+
 public class DetallesIncidenciaActivity extends AppCompatActivity {
 
     @Override
