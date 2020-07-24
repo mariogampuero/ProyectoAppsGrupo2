e56d65e2dcd6c2acb4bb4b45ce0eea8eb4a17c79
Renzo Manrique <renzo.manrique@pucp.pe>
2020-07-24 00:35:02 -0500

    Vista de detalles de la incidencia hecha, falta localizacion

--- app/src/main/java/com/example/proyectoappsgrupo2/activity/DetallesIncidenciaActivity.java
+++ app/src/main/java/com/example/proyectoappsgrupo2/activity/DetallesIncidenciaActivity.java
@@ -1,13 +1,18 @@
 package com.example.proyectoappsgrupo2.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.proyectoappsgrupo2.MainActivity;
 import com.example.proyectoappsgrupo2.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,10 +21,27 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class DetallesIncidenciaActivity extends FragmentActivity implements OnMapReadyCallback {
 
     GoogleMap map;
+
+    private String idIncidencia;
+    private TextView tituloDetalles;
+    private TextView descripcionDetalles;
+    private TextView estadoDetalles;
+    private TextView autorDetalles;
+    private ImageView imagenDetalles;
+    private static final String INCIDENCIAS = "Incidencias";
+    private StorageReference storageReference;
+    private String idPrueba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +50,66 @@ public class DetallesIncidenciaActivity extends FragmentActivity implements OnMa
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Intent inte = getIntent();
+        idIncidencia = inte.getStringExtra("idIncidencia");
+        idPrueba = "-MCymkPcCpFWC30XrPA6";
+
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference incidenciaRef = FirebaseDatabase.getInstance().getReference().child("Incidencias");
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        tituloDetalles = findViewById(R.id.titleDetalles);
+        descripcionDetalles = findViewById(R.id.descripDetalles);
+        estadoDetalles = findViewById(R.id.estadoDetalles);
+        autorDetalles = findViewById(R.id.autorDetalles);
+        imagenDetalles = findViewById(R.id.imageDetalles);
+
+        //DatabaseReference incidenciaRef = FirebaseDatabase.getInstance().getReference().child("Incidencias").child("-MCymkPcCpFWC30XrPA6");
+
+        incidenciaRef.addValueEventListener(new ValueEventListener() {
+            String title,descrip,aut;
+            int state;
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+               for (DataSnapshot keyId : dataSnapshot.getChildren()){
+                   Log.d("probando89", "uwu");
+                    if(keyId.getKey().equals(idPrueba)){
+                        Log.d("probando", "uwu");
+                        title = keyId.child("nombre").getValue(String.class);
+                        descrip = keyId.child("descripcion").getValue(String.class);
+                        aut = keyId.child("autor").getValue(String.class);
+                        state = keyId.child("estado").getValue(Integer.class);
+                        break;
+                    }
+                }
+
+              /*
+                title = dataSnapshot.child("nombre").getValue(String.class);
+                descrip = dataSnapshot.child("descripcion").getValue(String.class);
+                aut = dataSnapshot.child("autor").getValue(String.class);
+                state = dataSnapshot.child("estado").getValue(Integer.class);
+*/
+                tituloDetalles.setText(title);
+                descripcionDetalles.setText(descrip);
+                autorDetalles.setText(aut);
+                if(state == 0){
+                    estadoDetalles.setText("Pendiente");
+                }else {
+                    estadoDetalles.setText("Atendido");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        StorageReference fotoRef = storageReference.child("fotos/"+idPrueba);
+
+        Glide.with(this).load(fotoRef).into(imagenDetalles);
     }
 
     @Override
