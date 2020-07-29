6c62d022a31c78ff02c554032f51a3ec7584e999
Renzo Manrique <renzo.manrique@pucp.pe>
2020-07-28 20:26:20 -0500

    cambios en el inicio activity

--- app/src/main/java/com/example/proyectoappsgrupo2/activity/InicioActivity.java
+++ app/src/main/java/com/example/proyectoappsgrupo2/activity/InicioActivity.java
@@ -49,8 +49,8 @@ public class InicioActivity extends AppCompatActivity {
     private Menu menu;
     FirebaseAuth firebaseAuth;
     //DatabaseReference databaseReference;
-    //private ArrayList<Incidencia> listaIncidencias = new ArrayList<>();
-    //private Incidencia incidencia = new Incidencia();
+
+
     private String est;
     private String nombre;
     InicioListAdapter inicioListAdapter;
