4157c43256636d2657f139113a978fd3fba2bf93
Mario Ampuero <m.ampuero@pucp.pe>
2020-07-28 21:51:06 -0500

    Lógica de obtener el rol

--- app/src/main/java/com/example/proyectoappsgrupo2/activity/DetallesIncidenciaActivity.java
+++ app/src/main/java/com/example/proyectoappsgrupo2/activity/DetallesIncidenciaActivity.java
@@ -21,6 +21,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -41,7 +42,7 @@ public class DetallesIncidenciaActivity extends FragmentActivity implements OnMa
     private ImageView imagenDetalles;
     private static final String INCIDENCIAS = "Incidencias";
     private StorageReference storageReference;
-    private String idPrueba;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,9 +52,10 @@ public class DetallesIncidenciaActivity extends FragmentActivity implements OnMa
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+
+
         Intent inte = getIntent();
         idIncidencia = inte.getStringExtra("idIncidencia");
-        idPrueba = "-MCymkPcCpFWC30XrPA6";
 
         DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
         DatabaseReference incidenciaRef = FirebaseDatabase.getInstance().getReference().child("Incidencias");
@@ -65,17 +67,54 @@ public class DetallesIncidenciaActivity extends FragmentActivity implements OnMa
         autorDetalles = findViewById(R.id.autorDetalles);
         imagenDetalles = findViewById(R.id.imageDetalles);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         //DatabaseReference incidenciaRef = FirebaseDatabase.getInstance().getReference().child("Incidencias").child("-MCymkPcCpFWC30XrPA6");
+
+        //SE OBTIENEN LOS DATOS DE LA INCIDENCIA
 
         incidenciaRef.addValueEventListener(new ValueEventListener() {
             String title,descrip,aut;
-            int state;
+            String state;
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
                for (DataSnapshot keyId : dataSnapshot.getChildren()){
                    Log.d("probando89", "uwu");
-                    if(keyId.getKey().equals(idPrueba)){
+                    if(keyId.getKey().equals(idIncidencia)){
+                        Log.d("probando", "uwu");
+                        title = keyId.child("nombre").getValue(String.class);
+                        descrip = keyId.child("descripcion").getValue(String.class);
+                        aut = keyId.child("autor").getValue(String.class);
+                        state = keyId.child("estado").getValue(String.class);
+                        break;
+                    }
+                }
+
+                tituloDetalles.setText(title);
+                descripcionDetalles.setText(descrip);
+                autorDetalles.setText(aut);
+                estadoDetalles.setText(state);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //SE OBTIENE LA IMAGEN DE LA INCIDENCIA
+        StorageReference fotoRef = storageReference.child("fotos/"+idIncidencia);
+        Glide.with(this).load(fotoRef).into(imagenDetalles);
+
+        //SE OBTIENE EL ROL DEL USUARIO
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Usuarios");
+        /*databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot keyId : dataSnapshot.getChildren()){
+                    if(keyId.getKey().equals(firebaseAuth.getCurrentUser().getUid())){
                         Log.d("probando", "uwu");
                         title = keyId.child("nombre").getValue(String.class);
                         descrip = keyId.child("descripcion").getValue(String.class);
@@ -85,12 +124,6 @@ public class DetallesIncidenciaActivity extends FragmentActivity implements OnMa
                     }
                 }
 
-              /*
-                title = dataSnapshot.child("nombre").getValue(String.class);
-                descrip = dataSnapshot.child("descripcion").getValue(String.class);
-                aut = dataSnapshot.child("autor").getValue(String.class);
-                state = dataSnapshot.child("estado").getValue(Integer.class);
-*/
                 tituloDetalles.setText(title);
                 descripcionDetalles.setText(descrip);
                 autorDetalles.setText(aut);
@@ -105,11 +138,7 @@ public class DetallesIncidenciaActivity extends FragmentActivity implements OnMa
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
-        });
-
-        StorageReference fotoRef = storageReference.child("fotos/"+idPrueba);
-
-        Glide.with(this).load(fotoRef).into(imagenDetalles);
+        });*/
     }
 
     @Override
